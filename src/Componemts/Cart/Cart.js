79fe36c66d3469fd32394b0bd797f8339e88f4fc
Svet-home <aprelkovakatish@gmail.com>
2023-03-02 19:37:44 +0300

    first commit

--- src/Componemts/Cart/Cart.js
+++ src/Componemts/Cart/Cart.js
@@ -1,8 +1,16 @@
+import { useSelector } from "react-redux";
+import { getCartItems, getTotalPrice } from "../../redux/cartSlice";
+import CartItem from "./CartItem";
+
 const Cart = () => {
+    const cartItem =useSelector(getCartItems);
+    const totalPrice=useSelector(getTotalPrice);
     return(
         <div> 
            <img className="cartIcon" src="https://img.icons8.com/external-xnimrodx-lineal-color-xnimrodx/64/000000/external-shopping-cart-cyber-monday-xnimrodx-lineal-color-xnimrodx.png"/> 
-        </div>
+          <h3>TOTAL: ${totalPrice}</h3>
+           {cartItem.map(cartItem=> <CartItem cartItem={cartItem}/> )}
+          </div>
     )
 }
 export default Cart;
--- src/Componemts/DishesComponents/Dish.js
+++ src/Componemts/DishesComponents/Dish.js
@@ -1,12 +1,19 @@
+import ChangeQuantity from "../Cart/ChangeQuantity";
+import { useState } from "react";
+import{ useDispatch } from "react-redux";
+import {addItemToCart } from "../../redux/cartSlice";
+
 const Dish = ({dish}) => {
+    const[quantity,setQuantity] = useState(1);
+    const dispatch= useDispatch()
     return(
         <div>
             <img src={`./${dish.img}.jpg`}/>    
         <h2>{dish.name}</h2>  
         <p>$ {dish.price}</p>  
-        {/*To fix later*/}
-        <button>How many portion???</button>
-        <button>Add to cart</button> 
+       
+       <ChangeQuantity quantity={quantity} setQuantity ={setQuantity}/>
+        <button onClick={()=>{dispatch(addItemToCart({dish,quantity}))}}>Add to cart</button> 
         </div>
     )
 }
